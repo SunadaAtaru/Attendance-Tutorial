d52dd364d13e60fb6ec3e6b1dbd5ba7c9d969957
EC2 Default User <ec2-user@ip-172-31-8-10.ec2.internal>
2021-08-07 00:14:08 +0000

    出勤、退勤時間終了-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update, :show]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
+  
+
 
   def index
     @users = User.paginate(page: params[:page])
@@ -29,6 +31,11 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    unless user_signed_in? (current_user == @item.user)
+      redirect_to root_path
+    end
+
   end
 
   def update

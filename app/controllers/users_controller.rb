4b4cdac2110ce20b1389d60bd034e32707e19ab9
EC2 Default User <ec2-user@ip-172-31-8-10.ec2.internal>
2021-11-24 13:39:36 +0000

    seaechファイル書き直し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   
   
   def index
-      @users = User.where.not(id: 1).paginate(page: params[:page],per_page: 5 ).search(params[:search])
+      @users = User.where.not(id: 1).paginate(page: params[:page],per_page: 5 )#.search(params[:search])
       respond_to do |format|
       format.html do
       end
